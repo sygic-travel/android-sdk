e4d242a3910cce3357346607387b7aa1ad550082
Michal Murín <michal.murin@sygictravel.com>
2017-03-22 13:01:43 +0100

    user api key as constant, map move listener

--- app/src/main/java/com/sygic/travel/sdkdemo/MapsActivity.java
+++ app/src/main/java/com/sygic/travel/sdkdemo/MapsActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.sygic.travel.sdk.StSDK;
 import com.sygic.travel.sdk.contentProvider.api.Callback;
@@ -19,6 +20,8 @@ import com.sygic.travel.sdk.model.query.Query;
 
 import java.util.List;
 
+import static com.sygic.travel.sdk.contentProvider.api.StApiConstants.USER_X_API_KEY;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
 	private GoogleMap mMap;
@@ -27,22 +30,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
+
+		StSDK.initialize(USER_X_API_KEY, this);
+
 		// Obtain the SupportMapFragment and get notified when the map is ready to be used.
 		SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
 			.findFragmentById(R.id.map);
 		mapFragment.getMapAsync(this);
 	}
 
-
-	/**
-	 * Manipulates the map once available.
-	 * This callback is triggered when the map is ready to be used.
-	 * This is where we can add markers or lines, add listeners or move the camera. In this case,
-	 * we just add a marker near Sydney, Australia.
-	 * If Google Play services is not installed on the device, the user will be prompted to install
-	 * it inside the SupportMapFragment. This method will only be triggered once the user has
-	 * installed Google Play services and returned to the app.
-	 */
 	@Override
 	public void onMapReady(GoogleMap googleMap) {
 		mMap = googleMap;
@@ -52,6 +48,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
 		mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+		mMap.setOnCameraMoveListener(new GoogleMap.OnCameraMoveListener() {
+			@Override
+			public void onCameraMove() {
+				LatLngBounds bounds = mMap.getProjection().getVisibleRegion().latLngBounds;
+			}
+		});
+
 
 		/*******************************************************************
 		 * Simple usage of SDK
@@ -59,6 +62,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 		Query query = new Query(
 			null, null, "eating", null, "city:1", null, null, null, null, 20
 		);
+
 		Callback<List<Place>> placesBack = new Callback<List<Place>>() {
 			@Override
 			public void onSuccess(List<Place> data) {
@@ -96,11 +100,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 			}
 		};
 
-		String userXApiKey = "qBei674Bdt5lk2rTkphqP1jiXC7M96HR26BFNSGw"; //TODO only for testing
-		StSDK.initialize(userXApiKey, this);
 		StSDK.getInstance().getPlaces(query, placesBack);
-		StSDK.getInstance().getPlaceDetailed("poi:447", detailBack);
-		StSDK.getInstance().getPlaceMedia("poi:447", mediaBack);
+//		StSDK.getInstance().getPlaceDetailed("poi:447", detailBack);
+//		StSDK.getInstance().getPlaceMedia("poi:447", mediaBack);
 		/********************************************************************/
 	}
 }
