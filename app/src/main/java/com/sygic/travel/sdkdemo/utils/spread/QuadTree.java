50f77c4c6cded1cff211f0e642bdd1034bb5df84
Michal Murín <michal.murin@sygictravel.com>
2017-04-03 13:41:38 +0200

    base classes needed for spreading added

---
+++ app/src/main/java/com/sygic/travel/sdkdemo/utils/spread/QuadTree.java
@@ -0,0 +1,49 @@
+package com.sygic.travel.sdkdemo.utils.spread;
+
+import android.graphics.Rect;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by michal.murin on 3.4.2017.
+ */
+
+public class QuadTree {
+	public static final int FAIL_OUT_OF_BOUNDS = -2;
+	public static final int FAIL_INTERSECTS = -1;
+	public static final int INSERTED = 1;
+
+	private Intersection intersection;
+	private Rect mapRect;
+	private List<PlaceMeta> insertedPlaceMetas;
+
+	public QuadTree(Intersection intersection, Rect mapRect) {
+		this.intersection = intersection;
+		this.mapRect = mapRect;
+		insertedPlaceMetas = Collections.synchronizedList(new ArrayList<PlaceMeta>());
+	}
+
+	public boolean intersectsWithItems(PlaceMeta placeMeta){
+		for (PlaceMeta insertedPlaceMeta : insertedPlaceMetas){
+			if(intersection.intersects(insertedPlaceMeta, placeMeta)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int insert(PlaceMeta item){
+		Rect rect = intersection.getMaxRect(item);
+		if(!mapRect.contains(rect) && !Rect.intersects(mapRect, rect)){
+			return FAIL_OUT_OF_BOUNDS;
+		}
+
+		if(intersectsWithItems(item)){
+			return FAIL_INTERSECTS;
+		}
+		insertedPlaceMetas.add(item);
+		return INSERTED;
+	}
+}
