8d85c0cc94f79b25abe7417fa38ddac7d80f792d
Michal Murín <michal.murin@sygictravel.com>
2017-06-13 16:13:17 +0200

    get detailed places request added to StSDK, PlacesDetailedRepsonse removed - only PlacesResponse

--- sdk/src/main/java/com/sygic/travel/sdk/model/place/Detail.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/model/place/Detail.kt
@@ -1,21 +1,40 @@
 package com.sygic.travel.sdk.model.place
 
+import com.google.gson.annotations.SerializedName
 import com.sygic.travel.sdk.model.media.MainMedia
 
 /**
  * Place detailed information.
  */
 class Detail {
-
+    @SerializedName("tags")
     var tags: List<Tag>? = null
+
+    @SerializedName("description")
     var description: Description? = null
+
+    @SerializedName("address")
     var address: String? = null
+
+    @SerializedName("admission")
     var admission: String? = null
+
+    @SerializedName("duration")
     var duration: Int = 0
+
+    @SerializedName("email")
     var email: String? = null
+
+    @SerializedName("opening_hours")
     var openingHours: String? = null
+
+    @SerializedName("phone")
     var phone: String? = null
+
+    @SerializedName("main_media")
     var mainMedia: MainMedia? = null
+
+    @SerializedName("references")
     var references: List<Reference>? = null
 
 }
