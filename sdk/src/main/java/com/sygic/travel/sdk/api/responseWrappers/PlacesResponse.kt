5b852cd58b8d8954980add3c4ab019fe11f2e1c5
Michal Murín <michal.murin@sygictravel.com>
2017-06-13 16:13:17 +0200

    get detailed places request added to StSDK, PlacesDetailedRepsonse removed - only PlacesResponse

--- sdk/src/main/java/com/sygic/travel/sdk/api/responseWrappers/PlacesResponse.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/api/responseWrappers/PlacesResponse.kt
@@ -4,8 +4,8 @@ import com.google.gson.annotations.SerializedName
 import com.sygic.travel.sdk.model.place.Place
 
 /**
- * Response that contains a list of basic places data (without detailed data). Suitable for showing
- * places in a list or spread on a map.
+ * Response that contains a list of places data. Suitable for showing places in a list or
+ * spread on a map.
  */
 class PlacesResponse : StResponse() {
     private var data: Data? = null
