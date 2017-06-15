ad9503cbd4a8e461c9ae9cc51c5ce149a33560da
Michal Murín <michal.murin@sygictravel.com>
2017-06-15 09:22:54 +0200

    SDK: imports instead of full package paths

--- sdk/src/main/java/com/sygic/travel/sdk/api/StApi.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/api/StApi.kt
@@ -20,31 +20,31 @@ internal interface StApi {
     /*                                     GET                                     */
     /*-----------------------------------------------------------------------------*/
 
-    @retrofit2.http.GET("places/list")
+    @GET("places/list")
     fun getPlaces(
-            @retrofit2.http.Query("query") query: String?,
-            @retrofit2.http.Query("levels") levels: String?,
-            @retrofit2.http.Query("categories") categories: String?,
-            @retrofit2.http.Query(encoded = true, value = "map_tiles") mapTiles: String?,
-            @retrofit2.http.Query("map_spread") mapSpread: Int?,
-            @retrofit2.http.Query("bounds") bounds: String?,
-            @retrofit2.http.Query("tags") tags: String?,
-            @retrofit2.http.Query("parents") parents: String?,
-            @retrofit2.http.Query("limit") limit: Int?
-    ): rx.Observable<Result<PlacesResponse>>
+            @Query("query") query: String?,
+            @Query("levels") levels: String?,
+            @Query("categories") categories: String?,
+            @Query(encoded = true, value = "map_tiles") mapTiles: String?,
+            @Query("map_spread") mapSpread: Int?,
+            @Query("bounds") bounds: String?,
+            @Query("tags") tags: String?,
+            @Query("parents") parents: String?,
+            @Query("limit") limit: Int?
+    ): Observable<Result<PlacesResponse>>
 
-    @retrofit2.http.GET("places/{id}")
+    @GET("places/{id}")
     fun getPlaceDetailed(
-            @retrofit2.http.Path("id") id: String
-    ): rx.Observable<Result<PlaceDetailedResponse>>
+            @Path("id") id: String
+    ): Observable<Result<PlaceDetailedResponse>>
 
-    @retrofit2.http.GET("places")
+    @GET("places")
     fun getPlacesDetailed(
-            @retrofit2.http.Query(encoded = true, value = "ids") ids: String
-    ): rx.Observable<Result<PlacesResponse>>
+            @Query(encoded = true, value = "ids") ids: String
+    ): Observable<Result<PlacesResponse>>
 
-    @retrofit2.http.GET("places/{id}/media")
+    @GET("places/{id}/media")
     fun getPlaceMedia(
-            @retrofit2.http.Path("id") id: String
-    ): rx.Observable<Result<MediaResponse>>
+            @Path("id") id: String
+    ): Observable<Result<MediaResponse>>
 }
