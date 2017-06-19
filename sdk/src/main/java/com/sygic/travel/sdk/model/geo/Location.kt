411ad3bb7d2cd7b755e33e21646847786f6c2f7b
Michal Murín <michal.murin@sygictravel.com>
2017-06-19 14:04:16 +0200

    SDK: all redundant @SerializedName annotations removed from all public models

--- sdk/src/main/java/com/sygic/travel/sdk/model/geo/Location.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/model/geo/Location.kt
@@ -1,16 +1,9 @@
 package com.sygic.travel.sdk.model.geo
 
-import com.google.gson.annotations.SerializedName
-
 /**
  * Geographic location - latitude, longitude.
  */
 class Location {
-
-    @SerializedName("lat")
     var lat: Float = 0.toFloat()
-
-    @SerializedName("lng")
     var lng: Float = 0.toFloat()
-
 }
--- sdk/src/main/java/com/sygic/travel/sdk/model/media/MainMedia.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/model/media/MainMedia.kt
@@ -1,16 +1,9 @@
 package com.sygic.travel.sdk.model.media
 
-import com.google.gson.annotations.SerializedName
-
 /**
  * Place main media.
  */
 class MainMedia {
-
-    @SerializedName("usage")
     var usage: Usage? = null
-
-    @SerializedName("media")
     var media: List<Medium>? = null
-
 }
