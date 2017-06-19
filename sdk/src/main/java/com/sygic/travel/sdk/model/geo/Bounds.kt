89a45c5b49bf288c931c6784c07a070763d920b5
Michal Murín <michal.murin@sygictravel.com>
2017-06-19 14:04:16 +0200

    SDK: all redundant @SerializedName annotations removed from all public models

--- sdk/src/main/java/com/sygic/travel/sdk/model/geo/Bounds.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/model/geo/Bounds.kt
@@ -1,23 +1,12 @@
 package com.sygic.travel.sdk.model.geo
 
-import com.google.gson.annotations.SerializedName
-
 /**
- *
  * Geographical bounds - south, west, north, east.
  */
 class Bounds {
-
-    @SerializedName("north")
     var north: Float = 0.toFloat()
-
-    @SerializedName("east")
     var east: Float = 0.toFloat()
-
-    @SerializedName("south")
     var south: Float = 0.toFloat()
-
-    @SerializedName("west")
     var west: Float = 0.toFloat()
 
     fun toQueryString(): String {
