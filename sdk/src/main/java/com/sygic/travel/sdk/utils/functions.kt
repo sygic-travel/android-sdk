6f0bfe52a52ec85e03fd6029e2f55b1f570c3c35
Jan Skrasek <hrach.cz@gmail.com>
2017-11-22 13:08:57 +0100

    sdk: checkNotRunningOnMainThread() is internal

--- sdk/src/main/java/com/sygic/travel/sdk/utils/functions.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/utils/functions.kt
@@ -2,7 +2,7 @@ package com.sygic.travel.sdk.utils
 
 import android.os.Looper
 
-fun checkNotRunningOnMainThread() {
+internal fun checkNotRunningOnMainThread() {
 	if (Looper.myLooper() == Looper.getMainLooper()) {
 		throw IllegalStateException("Cannot access Sygic Travel SDK on the main thread since it may lock the UI for a long period of time.")
 	}
