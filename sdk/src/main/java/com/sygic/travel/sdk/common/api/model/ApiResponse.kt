850690b207b895ffd70e1eae70350c6096bb924b
Jan Skrasek <hrach.cz@gmail.com>
2017-11-24 16:37:53 +0100

    sdk/api: updated ApiResponse wrapper, added server_timestamp, removed error

--- sdk/src/main/java/com/sygic/travel/sdk/common/api/model/ApiResponse.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/common/api/model/ApiResponse.kt
@@ -1,18 +1,7 @@
 package com.sygic.travel.sdk.common.api.model
 
-import com.google.gson.annotations.SerializedName
-
-internal class ApiResponse<out T> {
-	@SerializedName("status_code")
-	val statusCode: Int = 0
-
-	@SerializedName("error")
-	val error: Error? = null
-
-	val data: T? = null
-
-	inner class Error {
-		val id: String? = null
-		val args: List<String>? = null
-	}
-}
+internal class ApiResponse<out T>(
+	val status_code: Int,
+	val server_timestamp: String,
+	val data: T?
+)
