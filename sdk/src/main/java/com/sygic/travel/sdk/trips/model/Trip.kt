fdfeba242b3d406c5b712c1abc3624d5de1e6efa
Jan Skrasek <hrach.cz@gmail.com>
2018-01-05 18:24:38 +0100

    sdk/trips: refactored creating Trip instance

--- sdk/src/main/java/com/sygic/travel/sdk/trips/model/Trip.kt
+++ sdk/src/main/java/com/sygic/travel/sdk/trips/model/Trip.kt
@@ -7,18 +7,49 @@ import java.util.UUID
  * It contains all metadata and days' definitions.
  */
 @Suppress("ConvertSecondaryConstructorToPrimary")
-class Trip : TripInfo {
+class Trip internal constructor(id: String) : TripInfo(id) {
+	companion object {
+		/**
+		 * Creates a new trip instance with a local ID.
+		 */
+		fun create(): Trip {
+			return Trip(UUID.randomUUID().toString())
+		}
+
+		/**
+		 * Creates a new trip with preloaded internal properties.
+		 * This method is internal but open.
+		 */
+		fun createWithInternalProperties(
+			tripId: String,
+			url: String,
+			privileges: TripPrivileges,
+			isDeleted: Boolean,
+			media: TripMedia?,
+			updatedAt: Long,
+			isChanged: Boolean,
+			daysCount: Int,
+			ownerId: String?,
+			version: Int
+		): Trip {
+			val trip = Trip(tripId)
+			trip.url = url
+			trip.privileges = privileges
+			trip.isDeleted = isDeleted
+			trip.media = media
+			trip.updatedAt = updatedAt
+			trip.isChanged = isChanged
+			trip.daysCount = daysCount
+			trip.ownerId = ownerId
+			trip.version = version
+			return trip
+		}
+	}
+
 	var days = mutableListOf<TripDay>()
 	override var daysCount: Int = 0
 		get() = days.size
 		internal set
-
-	/**
-	 * Creates a new trip instance with a local ID.
-	 */
-	constructor() : super(UUID.randomUUID().toString())
-
-	internal constructor(id: String) : super(id)
 
 	fun getPlaceIds(): Set<String> {
 		return days.map { it.getPlaceIds() }.flatten().toSet()
