625df9e12342b7861f17c5ac01760b0ee8e56194
Jan Skrasek <hrach.cz@gmail.com>
2018-02-06 15:09:00 +0100

    exntension functions as internal

--- src/main/java/com/sygic/travel/sdk/utils/extensions.kt
+++ src/main/java/com/sygic/travel/sdk/utils/extensions.kt
@@ -2,14 +2,14 @@ package com.sygic.travel.sdk.utils
 
 import java.util.Date
 
-fun Long?.asDate(): Date? {
+internal fun Long?.asDate(): Date? {
 	return when (this) {
 		null -> null
 		else -> Date(this * 1000)
 	}
 }
 
-val Date?.timeSeconds: Long?
+internal val Date?.timeSeconds: Long?
 	get() {
 		return when (this) {
 			null -> null
