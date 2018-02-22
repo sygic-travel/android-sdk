542352b23a2225f988534380f6ade50dca60d0d2
Jan Skrasek <hrach.cz@gmail.com>
2018-02-22 16:17:58 +0100

    sdk: make clientId optional, lock Favorites, Session, Synchronization, Trips module only for user-data configurations

--- src/main/java/com/sygic/travel/sdk/Sdk.kt
+++ src/main/java/com/sygic/travel/sdk/Sdk.kt
@@ -2,12 +2,12 @@ package com.sygic.travel.sdk
 
 import android.content.Context
 import com.github.salomonbrys.kodein.instance
-import com.sygic.travel.sdk.session.facade.SessionFacade
 import com.sygic.travel.sdk.common.di.KodeinSetup
 import com.sygic.travel.sdk.directions.facades.DirectionsFacade
 import com.sygic.travel.sdk.events.facades.EventsFacade
 import com.sygic.travel.sdk.favorites.facade.FavoritesFacade
 import com.sygic.travel.sdk.places.facade.PlacesFacade
+import com.sygic.travel.sdk.session.facade.SessionFacade
 import com.sygic.travel.sdk.synchronization.facades.SynchronizationFacade
 import com.sygic.travel.sdk.tours.facade.ToursFacade
 import com.sygic.travel.sdk.trips.facades.TripsFacade
@@ -19,13 +19,47 @@ class Sdk(
 	applicationContext: Context,
 	sdkConfig: SdkConfig
 ) {
-	val directionsFacade: DirectionsFacade by lazy { kodein.instance<DirectionsFacade>() }
-	val eventsFacade: EventsFacade by lazy { kodein.instance<EventsFacade>() }
-	val favoritesFacade: FavoritesFacade by lazy { kodein.instance<FavoritesFacade>() }
-	val placesFacade: PlacesFacade by lazy { kodein.instance<PlacesFacade>() }
-	val sessionFacade: SessionFacade by lazy { kodein.instance<SessionFacade>() }
-	val synchronizationFacade: SynchronizationFacade by lazy { kodein.instance<SynchronizationFacade>() }
-	val toursFacade: ToursFacade by lazy { kodein.instance<ToursFacade>() }
-	val tripsFacade: TripsFacade by lazy { kodein.instance<TripsFacade>() }
+	val directionsFacade: DirectionsFacade by lazy {
+		kodein.instance<DirectionsFacade>()
+	}
+
+	val eventsFacade: EventsFacade by lazy {
+		kodein.instance<EventsFacade>()
+	}
+
+	val favoritesFacade: FavoritesFacade by lazy {
+		checkUserDataSupport("Favorites")
+		kodein.instance<FavoritesFacade>()
+	}
+
+	val placesFacade: PlacesFacade by lazy {
+		kodein.instance<PlacesFacade>()
+	}
+
+	val sessionFacade: SessionFacade by lazy {
+		checkUserDataSupport("Session")
+		kodein.instance<SessionFacade>()
+	}
+
+	val synchronizationFacade: SynchronizationFacade by lazy {
+		checkUserDataSupport("Synchronization")
+		kodein.instance<SynchronizationFacade>()
+	}
+
+	val toursFacade: ToursFacade by lazy {
+		kodein.instance<ToursFacade>()
+	}
+
+	val tripsFacade: TripsFacade by lazy {
+		checkUserDataSupport("Trips")
+		kodein.instance<TripsFacade>()
+	}
+
 	private val kodein = KodeinSetup.setupKodein(applicationContext, sdkConfig)
+
+	private fun checkUserDataSupport(module: String) {
+		if (!kodein.instance<Boolean>("userDataSupported")) {
+			throw IllegalStateException("$module module can be used only with enabled user-data support. To enable it, configure Sdk with clientId.")
+		}
+	}
 }
