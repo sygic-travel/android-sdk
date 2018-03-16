d327de964e290f2e22772e16d5a3210844bae367
Jan Skrasek <hrach.cz@gmail.com>
2018-03-16 14:51:14 +0100

    synchronization: fixed mistyped createdTripsIdsMapping property

--- src/main/java/com/sygic/travel/sdk/synchronization/model/SynchronizationResult.kt
+++ src/main/java/com/sygic/travel/sdk/synchronization/model/SynchronizationResult.kt
@@ -3,7 +3,7 @@ package com.sygic.travel.sdk.synchronization.model
 class SynchronizationResult(
 	val changedTripIds: MutableSet<String> = mutableSetOf(),
 	val changedFavoriteIds: MutableSet<String> = mutableSetOf(),
-	val cratedTripIdsMapping: MutableMap<String, String> = mutableMapOf(),
+	val createdTripIdsMapping: MutableMap<String, String> = mutableMapOf(),
 	var success: Boolean = true,
 	var exception: Exception? = null
 )
