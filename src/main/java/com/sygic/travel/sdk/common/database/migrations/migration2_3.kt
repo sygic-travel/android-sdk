40247ddc0cadd7f1019b2f1c58d9f84749a24323
Jan Skrasek <hrach.cz@gmail.com>
2018-04-24 13:12:14 +0200

    favorites: delete invalid favorites synced from sygic travel backend

--- src/main/java/com/sygic/travel/sdk/common/database/migrations/migration2_3.kt
+++ src/main/java/com/sygic/travel/sdk/common/database/migrations/migration2_3.kt
@@ -6,5 +6,6 @@ import android.arch.persistence.room.migration.Migration
 internal val migration2_3 = object : Migration(2, 3) {
 	override fun migrate(database: SupportSQLiteDatabase) {
 		database.execSQL("UPDATE `trip_day_items` SET `transport_mode` = 'PUBLIC_TRANSPORT' WHERE `transport_mode` = 'PUBLIC_TRANSIT'")
+		database.execSQL("DELETE FROM `favorites` WHERE `id` LIKE ':%'")
 	}
 }
