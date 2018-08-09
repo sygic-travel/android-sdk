c40fa201132b7eba8fb72545dbbd11e10d1202a1
Jan Skrasek <hrach.cz@gmail.com>
2018-08-09 17:15:25 +0200

    geo: added LatLngBounds.getCenter()

--- src/main/java/com/sygic/travel/sdk/places/model/geo/LatLngBounds.kt
+++ src/main/java/com/sygic/travel/sdk/places/model/geo/LatLngBounds.kt
@@ -9,6 +9,19 @@ data class LatLngBounds(
 	val northeast: LatLng,
 	val southwest: LatLng
 ) : Parcelable, Serializable {
+	/**
+	 * Returns the center of the bounding box. The center is simply the average of the coordinates.
+	 */
+	fun getCenter(): LatLng {
+		val lat = (southwest.lat + northeast.lat) / 2.0
+		val lng = if (southwest.lng <= northeast.lng) {
+			(southwest.lng + northeast.lng) / 2.0
+		} else {
+			(southwest.lng + northeast.lng + 360.0) / 2.0
+		}
+		return LatLng(lat, lng)
+	}
+
 	internal fun toApiQuery(): String {
 		return "${southwest.lat},${southwest.lng},${northeast.lat},${northeast.lng}"
 	}
