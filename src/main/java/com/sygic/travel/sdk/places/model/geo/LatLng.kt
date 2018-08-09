0017ec1b10c5bb3d5661d2ff9755851f11cc919b
Jan Skrasek <hrach.cz@gmail.com>
2018-08-09 17:21:47 +0200

    geo: added LatLng.distanceTo()

--- src/main/java/com/sygic/travel/sdk/places/model/geo/LatLng.kt
+++ src/main/java/com/sygic/travel/sdk/places/model/geo/LatLng.kt
@@ -1,5 +1,6 @@
 package com.sygic.travel.sdk.places.model.geo
 
+import android.location.Location
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import java.io.Serializable
@@ -8,4 +9,19 @@ import java.io.Serializable
 data class LatLng(
 	val lat: Double,
 	val lng: Double
-) : Parcelable, Serializable
+) : Parcelable, Serializable {
+	/**
+	 * Returns the approximate shortest distance to the point in meters. Uses WGS84 ellipsoid.
+	 */
+	fun distanceTo(point: LatLng): Float {
+		val results = floatArrayOf(0f, 0f, 0f)
+		Location.distanceBetween(
+			lat,
+			lng,
+			point.lat,
+			point.lng,
+			results
+		)
+		return results[0]
+	}
+}
